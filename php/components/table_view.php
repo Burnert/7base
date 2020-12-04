50a7ca9e08a37b36436d2fa24244b75dfd44f9a1
Burnert <burnertski@gmail.com>
2020-12-04 10:47:12 +0100

    Added add entry button

--- php/components/table_view.php
+++ php/components/table_view.php
@@ -1,36 +1,49 @@
 <?php
 
-function table_view($rows, $columns = null) {
+function table_view($rows, $columns) {
 ?>
   <div class="table-wrapper">
   <table class="table-view">
-  <?php if ($columns):
-    echo "<tr>";
-    foreach ($columns as $column) {
-?>
-      <th><?php echo $column["Field"] ?></th>
-<?php
-    }
-    echo "</tr>";
-  endif;
-
-  if ($rows):
-    foreach ($rows as $row) {
+    <tbody>
+    <?php if ($columns):
       echo "<tr>";
-      foreach ($row as $value) {
-        echo "<td>";
-        if ($value) {
-          echo $value;
-        }
-        else {
-          echo "<i>NULL</i>";
-        }
-        echo "</td>";
-      } 
-      echo "</tr>";
-    }
-  endif;
+      foreach ($columns as $column) {
 ?>
+        <th><?php echo $column["Field"] ?></th>
+<?php
+      }
+      echo "</tr>";
+    endif;
+
+    if ($rows):
+      foreach ($rows as $row) {
+        echo "<tr>";
+        foreach ($row as $value) {
+          echo "<td>";
+          if ($value) {
+            echo $value;
+          }
+          else {
+            echo "<i>NULL</i>";
+          }
+          echo "</td>";
+        } 
+        echo "</tr>";
+      }
+    endif;
+?>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colspan="<?php echo count($columns) ?>">
+          <div>
+            <button type="button" class="soft" id="b-add-entry">
+              <i class="material-icons">add</i>
+            </button>
+          </div>
+        </td>
+      </tr>
+    </tfoot>
   </table>
   </div>
 <?php
