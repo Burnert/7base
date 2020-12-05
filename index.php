222d3178cf6c628775d17c8018f63aa603c535a9
Burnert <burnertski@gmail.com>
2020-12-05 15:53:17 +0100

    Moved table view style to separate file

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ require_once("./php/components/table_view.php");
   <link rel="stylesheet" href="./css/normalize.css">
   <link rel="stylesheet" href="./css/style.css">
   <link rel="stylesheet" href="./css/components/menu.css">
+  <link rel="stylesheet" href="./css/components/table_view.css">
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap" rel="stylesheet">
